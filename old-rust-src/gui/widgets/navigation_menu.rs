b3bd484f159d1ee2e2ccc3dfbff552a0cb727143
Glenn D <glenn@code-kungfu.com>
2024-03-17 15:16:47 +0100

    Housekeeping

---
+++ old-rust-src/gui/widgets/navigation_menu.rs
@@ -0,0 +1,121 @@
+pub use crate::core::sync::Phone;
+use crate::core::theme::Theme;
+use crate::core::update::{SelfUpdateState, SelfUpdateStatus};
+pub use crate::gui::views::about::Message as AboutMessage;
+pub use crate::gui::views::list::{List as AppsView, LoadingState as ListLoadingState};
+use crate::gui::{style, Message};
+use iced::widget::{button, container, pick_list, row, text, tooltip, Space, Text};
+use iced::{alignment, font, Alignment, Element, Font, Length, Renderer};
+
+/// resources/assets/icons.ttf, loaded in [`crate::gui::UadGui`]
+pub const ICONS: Font = Font {
+    family: font::Family::Name("icomoon"),
+    ..Font::DEFAULT
+};
+
+pub fn nav_menu<'a>(
+    device_list: &'a Vec<Phone>,
+    selected_device: Option<Phone>,
+    apps_view: &AppsView,
+    self_update_state: &SelfUpdateState,
+) -> Element<'a, Message, Renderer<Theme>> {
+    let apps_refresh_btn = button(
+        Text::new("\u{E900}")
+            .font(ICONS)
+            .width(22)
+            .horizontal_alignment(alignment::Horizontal::Center),
+    )
+    .on_press(Message::RefreshButtonPressed)
+    .padding(5)
+    .style(style::Button::Refresh);
+
+    let apps_refresh_tooltip = tooltip(apps_refresh_btn, "Refresh apps", tooltip::Position::Bottom)
+        .style(style::Container::Tooltip)
+        .gap(4);
+
+    let reboot_btn = button("Reboot")
+        .on_press(Message::RebootButtonPressed)
+        .padding(5)
+        .style(style::Button::Refresh);
+
+    #[allow(clippy::option_if_let_else)]
+    let uad_version_text = if let Some(r) = &self_update_state.latest_release {
+        if self_update_state.status == SelfUpdateStatus::Updating {
+            Text::new("Updating please wait...")
+        } else {
+            Text::new(format!(
+                "Update available: {} -> {}",
+                env!("CARGO_PKG_VERSION"),
+                r.tag_name
+            ))
+        }
+    } else {
+        Text::new(format!("v{}", env!("CARGO_PKG_VERSION")))
+    };
+
+    let update_btn = if self_update_state.latest_release.is_some() {
+        button("Update")
+            .on_press(Message::AboutAction(AboutMessage::DoSelfUpdate))
+            .padding(5)
+            .style(style::Button::SelfUpdate)
+    } else {
+        button("").height(0).width(0).style(style::Button::Hidden)
+    };
+
+    let apps_btn = button("Apps")
+        .on_press(Message::AppsPress)
+        .padding(5)
+        .style(style::Button::Primary);
+
+    let about_btn = button("About")
+        .on_press(Message::AboutPressed)
+        .padding(5)
+        .style(style::Button::Primary);
+
+    let settings_btn = button("Settings")
+        .on_press(Message::SettingsPressed)
+        .padding(5)
+        .style(style::Button::Primary);
+
+    let device_list_text = match apps_view.loading_state {
+        ListLoadingState::FindingPhones => text("Finding connected devices..."),
+        _ => text("No devices/emulators found"),
+    };
+
+    let row = match selected_device {
+        Some(phone) => row![
+            reboot_btn,
+            apps_refresh_tooltip,
+            pick_list(device_list, Some(phone), Message::DeviceSelected,),
+            Space::new(Length::Fill, Length::Shrink),
+            uad_version_text,
+            update_btn,
+            apps_btn,
+            about_btn,
+            settings_btn,
+        ]
+        .width(Length::Fill)
+        .align_items(Alignment::Center)
+        .spacing(10),
+        None => row![
+            reboot_btn,
+            apps_refresh_tooltip,
+            device_list_text,
+            Space::new(Length::Fill, Length::Shrink),
+            uad_version_text,
+            update_btn,
+            apps_btn,
+            about_btn,
+            settings_btn,
+        ]
+        .width(Length::Fill)
+        .align_items(Alignment::Center)
+        .spacing(10),
+    };
+
+    container(row)
+        .width(Length::Fill)
+        .padding(10)
+        .style(style::Container::Frame)
+        .into()
+}
---
+++ old-rust-src/main.rs
@@ -0,0 +1,92 @@
+#![windows_subsystem = "windows"]
+#[macro_use]
+extern crate log;
+
+use crate::core::utils::setup_uad_dir;
+use fern::{
+    colors::{Color, ColoredLevelConfig},
+    FormatCallback,
+};
+use log::Record;
+use static_init::dynamic;
+use std::path::PathBuf;
+use std::{fmt::Arguments, fs::OpenOptions};
+
+mod core;
+mod gui;
+
+#[dynamic]
+static CONFIG_DIR: PathBuf = setup_uad_dir(dirs::config_dir());
+
+#[dynamic]
+static CACHE_DIR: PathBuf = setup_uad_dir(dirs::cache_dir());
+
+fn main() -> iced::Result {
+    setup_logger().expect("setup logging");
+    gui::UadGui::start()
+}
+
+pub fn setup_logger() -> Result<(), fern::InitError> {
+    attach_windows_console();
+
+    let colors = ColoredLevelConfig::new().info(Color::Green);
+
+    let make_formatter = |use_colors: bool| {
+        move |out: FormatCallback, message: &Arguments, record: &Record| {
+            out.finish(format_args!(
+                "{} {} [{}:{}] {}",
+                chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                if use_colors {
+                    format!("{:5}", colors.color(record.level()))
+                } else {
+                    format!("{:5}", record.level().to_string())
+                },
+                record.file().unwrap_or("?"),
+                record.line().map(|l| l.to_string()).unwrap_or_default(),
+                message
+            ));
+        }
+    };
+
+    let default_log_level = log::LevelFilter::Warn;
+    let log_file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .truncate(false)
+        .open(CACHE_DIR.join(format!("UAD_{}.log", chrono::Local::now().format("%Y%m%d"))))?;
+
+    let file_dispatcher = fern::Dispatch::new()
+        .format(make_formatter(false))
+        .level(default_log_level)
+        // Rust compiler makes module names use _ instead of -
+        .level_for("uad_ng", log::LevelFilter::Debug)
+        .chain(log_file);
+
+    let stdout_dispatcher = fern::Dispatch::new()
+        .format(make_formatter(true))
+        .level(default_log_level)
+        // Rust compiler makes module names use _ instead of -
+        .level_for("uad_ng", log::LevelFilter::Warn)
+        .chain(std::io::stdout());
+
+    fern::Dispatch::new()
+        .chain(stdout_dispatcher)
+        .chain(file_dispatcher)
+        .apply()?;
+
+    Ok(())
+}
+
+/// (Windows) Allow the application to display logs to the terminal
+/// regardless if it was compiled with `windows_subsystem = "windows"`.
+///
+/// This is a no-op when compiled to non-windows targets.
+fn attach_windows_console() {
+    #[cfg(target_os = "windows")]
+    {
+        use win32console::console::WinConsole;
+
+        const ATTACH_PARENT_PROCESS: u32 = 0xFFFFFFFF;
+        let _ = WinConsole::attach_console(ATTACH_PARENT_PROCESS);
+    }
+}
